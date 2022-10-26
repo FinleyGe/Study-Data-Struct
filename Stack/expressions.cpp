d03c0ad949854d906e7bec336c37c6e7ec8b7379
FinleyGe <m13203533462@163.com>
2022-10-26 19:16:26 +0800

    feat: midfix to postfix exporession

--- Stack/expressions.cpp
+++ Stack/expressions.cpp
@@ -1,46 +1,72 @@
-#include "Stack.cpp"
+#include <iostream>
+#include <list>
+#include <stack>
 #include <string>
 using namespace std;
-string infixToPostfix(const string &s) {
-  Stack<char> st;
-  string res;
-
-  for (auto &&c : s) {
-    if (c == ' ')
-      continue;
-    if (c == '(') {
-      st.push('(');
-    } else if (c == ')') {
-      while (!st.empty()) {
-        char c = st.top();
-        if (c == '(') {
-          st.pop();
+// transform midfix expression to postfix expression.
+// the number must be a single digit.
+// the expression must be valid.
+// only + - * are supported.
+string trans(const string &raw) {
+  stack<char> stk;            // operator stack
+  string res;                 // postfix expression
+  for (const char &i : raw) { // analyse every char
+    if (i == '(') {           // when it is (, just push it into stack.
+      stk.push(i);
+    } else if (i == ')') { // when it is ), pop all operators until (.
+      while (!stk.empty()) {
+        if (stk.top() == '(') {
+          stk.pop();
           break;
+        } else {
+          res += stk.top();
+          stk.pop();
         }
-        res.push_back(c);
-        st.pop();
       }
-    } else if (c == '+' || c == '-') {
-      st.push(c);
-    } else if (c == '*') {
-      while (!st.empty()) {
-        char c = st.top();
-        if (c == '(') {
+    } else if (i == '+' or i == '-' or i == '*') { // when it is operator, pop
+                                                   // all operators which have
+                                                   // lower priority.
+      while (!stk.empty()) {
+        if (stk.top() == '(' or (i == '*' and stk.top() != '*'))
           break;
-        }
-        st.pop();
-        res.push_back(c);
+        res += stk.top();
+        stk.pop();
       }
-    } else {
-      res += c;
+      stk.push(i);
+    } else { // when it is number, just push it into result.
+      res += i;
     }
+  }
+  // after analysing all chars, pop all operators in stack.
+  while (!stk.empty()) {
+    res += stk.top();
+    stk.pop();
   }
   return res;
 }
 
 int main() {
-  string s;
-  cin >> s;
-  cout << infixToPostfix(s) << endl;
+
+  list<string> listIn = {
+      "(1+5-4)*(6-9)",
+      "2+3*5+(6+9*4)*4",
+      "1+2+3",
+      "1+2*5+1",
+  };
+  list<string> listOut;
+  cout << "midfix expressions:" << endl;
+  for (const string &i : listIn) {
+    cout << i << endl;
+  }
+
+  for (auto &&i : listIn) {
+    listOut.push_back(trans(i));
+  }
+
+  cout << "postfix expressions:" << endl;
+
+  for (auto &&i : listOut) {
+    cout << i << endl;
+  }
   return 0;
 }
